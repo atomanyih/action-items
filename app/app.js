cab3fedf270656dad168c25bace1099539867c68
August Toman-Yih <atomanyih@pivotal.io>
2015-08-04 19:04:54 -0700

    use some pui react

--- app/app.js
+++ app/app.js
@@ -1,14 +1,20 @@
 import React from "react";
+import UI from "pui-react-lists";
 
-var Whatdup = React.createClass({
+var ActionItems = React.createClass({
   render() {
     return (
-      <h1> HEY YO </h1>
+      <div className="actionItems txt-c">
+        <h1 className="em-high em-alt">Do it</h1>
+        <UI.GroupList>
+          <UI.ListItem>What</UI.ListItem>
+        </UI.GroupList>
+      </div>
     )
   }
 });
 
 React.render(
-  <Whatdup />,
-  document.body
+  <ActionItems />,
+  document.getElementById('root')
 );
