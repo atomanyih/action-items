84dac49e8e6b917838df6134c0b7bbbd5562ba5e
August Toman-Yih <atomanyih@pivotal.io>
2015-08-04 18:33:30 -0700

    Add webpack, babel, react, etc

---
+++ app/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+var Whatdup = React.createClass({
+  render() {
+    return (
+      <h1> HEY YO </h1>
+    )
+  }
+});
+
+React.render(
+  <Whatdup />,
+  document.body
+);
